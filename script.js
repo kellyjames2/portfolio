d4053e54337b3c71afca49d8baebb28eb2fea655
kellyjames2 <135234517+kellyjames2@users.noreply.github.com>
2025-07-10 14:09:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  fetch('https://github.com/kellyjames2/portfolio/blob/main/assets/abstract.txt')
+  fetch('assets/abstract.txt')
     .then(response => response.text())
     .then(data => {
       document.getElementById('abstract-content').innerText = data;
